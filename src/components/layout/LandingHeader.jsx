ddf6b932550a45c52d92e4372d1ee4adc992468b
TiffanyStPierre <stpierre.tiffany@gmail.com>
2023-02-26 14:24:54 -0700

    Completed landing page

--- src/components/layout/LandingHeader.jsx
+++ src/components/layout/LandingHeader.jsx
@@ -1,9 +1,11 @@
+import {Link} from 'react-router-dom';
+
 export default function LandingHeader() {
     return (
         <div className="landing-header">
             <h1 className="landing-h1">The Mind Bender</h1>
             <p className="landing-subtitle">Test your trivia knowledge with this fun trivia game!</p>
-            <a>Start Game</a>
+            <Link to='/quiz' className="start-game">Start Game</Link>
         </div>
     )
 }
